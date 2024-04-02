e47e07b0043dcab5d1979af4d365644c9ee9c1ce
Ayomide Akintan <emzyakints2005@gmail.com>
2024-04-02 19:10:48 +0100

    added isLoading to frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,7 @@ function App() {
   });
   const [result, setResult] = useState(null);
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false)
 
   const currencyCodes = ["AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN", "BAM", "BBD", "BDT", "BGN", "BHD", "BIF", "BMD", "BND", "BOB", "BRL", "BSD", "BTN", "BWP", "BYN", "BZD", "CAD", "CDF", "CHF", "CLP", "CNY", "COP", "CRC", "CUP", "CVE", "CZK", "DJF", "DKK", "DOP", "DZD", "EGP", "ERN", "ETB", "EUR", "FJD", "FKP", "FOK", "GBP", "GEL", "GGP", "GHS", "GIP", "GMD", "GNF", "GTQ", "GYD", "HKD", "HNL", "HRK", "HTG", "HUF", "IDR", "ILS", "IMP", "INR", "IQD", "IRR", "ISK", "JEP", "JMD", "JOD", "JPY", "KES", "KGS", "KHR", "KID", "KMF", "KRW", "KWD", "KYD", "KZT", "LAK", "LBP", "LKR", "LRD", "LSL", "LYD", "MAD", "MDL", "MGA", "MKD", "MMK", "MNT", "MOP", "MRU", "MUR", "MVR", "MWK", "MXN", "MYR", "MZN", "NAD", "NGN", "NIO", "NOK", "NPR", "NZD", "OMR", "PAB", "PEN", "PGK", "PHP", "PKR", "PLN", "PYG", "QAR", "RON", "RSD", "RUB", "RWF", "SAR", "SBD", "SCR", "SDG", "SEK", "SGD", "SHP", "SLE", "SOS", "SRD", "SSP", "STN", "SYP", "SZL", "THB", "TJS", "TMT", "TND", "TOP", "TRY", "TTD", "TVD", "TWD", "TZS", "UAH", "UGX", "USD", "UYU", "UZS", "VES", "VND", "VUV", "WST", "XAF", "XCD", "XDR", "XOF", "XPF", "YER", "ZAR", "ZMW", "ZWL"];
 
@@ -26,6 +27,7 @@ function App() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setIsLoading(true)
     // http request
     try {
       const response = await axios.post(
@@ -34,8 +36,10 @@ function App() {
       )
       setResult(response?.data)
       setError("")
+      setIsLoading(false)
     } catch (error) {
       setError('Error', error?.response ? error?.response?.data : error?.message)
+      setIsLoading(false)
     }
   };
 
@@ -85,7 +89,7 @@ function App() {
             type="number"
             className="input"
           />
-          <button type="submit" className="submit-btn">
+          <button type="submit" className={isLoading ? "submit-btn-loading" : "submit-btn"} disabled={isLoading}>
             Convert
           </button>
         </form>
